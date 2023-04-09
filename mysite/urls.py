f7b12f0c5f17822f8d79ff256da05fc339899cd0
NazariZahra <nazarii.zahraa@gmail.com>
2023-04-09 16:25:37 +0430

    phase1

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    ##in haman admini ast ke tu address bar baraye dastresi be panel user ha mizadim.
+    path('', include('blog.urls')),
 ]
